eadca31919539eda8836cda584eb1ba3552f5f15
jstick2599 <58639465+jstick2599@users.noreply.github.com>
2021-05-07 00:10:14 -0700

    resized and repositioned pacman

--- pacman/pacman.cpp
+++ pacman/pacman.cpp
@@ -1,11 +1,11 @@
 #include "object.h"
 
 pacman::pacman() :
-    drawPac(new QGraphicsPixmapItem(QPixmap(":/imgPac.png").scaled(45,55)))
+    drawPac(new QGraphicsPixmapItem(QPixmap(":/imgPac.png").scaled(25,28)))
 {
 
-    drawPac->setPos(QPointF(0,0) - QPointF(drawPac->boundingRect().width(),
-                    drawPac->boundingRect().height() - 200)); // x,y + move amount
+    drawPac->setPos(QPointF(0,0) - QPointF(drawPac->boundingRect().width()+170,
+                    drawPac->boundingRect().height() + 163)); // x,y + move amount
 
 
     addToGroup(drawPac);
